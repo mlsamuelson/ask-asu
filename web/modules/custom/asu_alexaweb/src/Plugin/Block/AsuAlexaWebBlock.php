d95989208d8cb7efa08a870299f591156479e4fb
mlsamuelson <mlsamuel@asu.edu>
2018-03-08 13:21:02 -0700

    ALEXADEV-1917 - templating for block, initial test

--- web/modules/custom/asu_alexaweb/src/Plugin/Block/AsuAlexaWebBlock.php
+++ web/modules/custom/asu_alexaweb/src/Plugin/Block/AsuAlexaWebBlock.php
@@ -8,9 +8,9 @@ use Drupal\Core\Block\BlockBase;
  * Provides an 'ASU AlexaWeb' iframe Block.
  *
  * @Block(
- *   id = "asu_alexaweb",
+ *   id = "asu_alexaweb_block",
  *   admin_label = @Translation("ASU AlexaWeb"),
- *   category = @Translation("ASU AlexaWeb"),
+ *   category = @Translation("ASU"),
  * )
  */
 class AsuAlexaWebBlock extends BlockBase {
